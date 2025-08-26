ad608216b05327092f605d094ebbaab13a0e7639
Miquéias Belarmino <miqueiascostabac@gmail.com>
2025-08-26 04:22:15 -0300

    refactor (orders): rename update route

--- api/src/routes/order-routes.js
+++ api/src/routes/order-routes.js
@@ -5,6 +5,6 @@ const orderController = require('../controllers/order-controller');
 router.post('/orders', orderController.create);
 router.get('/orders/:id', orderController.findById);
 router.get('/orders', orderController.findAll);
-router.put('/orders/:id', orderController.updateStatus);
+router.put('/orders/:id/status', orderController.updateStatus);
 
 module.exports = router;
